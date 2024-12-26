be269a1a10296e096ec79f5d0c484859e1575bf3
Helder Cambuta <heldercambuta44@gmail.com>
2024-12-26 03:49:22 +0100

    Initial Commit

---
+++ NearbyTests/NearbyTests.swift
@@ -0,0 +1,17 @@
+//
+//  NearbyTests.swift
+//  NearbyTests
+//
+//  Created by Hélder Cambuta on 26/12/24.
+//
+
+import Testing
+@testable import Nearby
+
+struct NearbyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
